f03b16a13fee29a75186b209fc00af4b57e453de
chenming <chenming@xjgreat.com>
2018-07-24 17:55:04 +0800

    computed and watcher

---
+++ properties.js
@@ -0,0 +1,29 @@
+var vm = new Vue({
+  el: '#example',
+  data: {
+    message: 'Hello'
+  },
+  computed: {
+    // a computed getter
+    reversedMessage: function () {
+      // `this` points to the vm instance
+      return this.message.split('').reverse().join('')
+    }
+  }
+});
+
+// computed
+var vm = new Vue({
+  el: '#demo',
+  data: {
+    firstName: 'Foo',
+    lastName: 'Bar'
+  },
+  computed: {
+    fullName: function () {
+      return this.firstName + ' ' + this.lastName
+    }
+  }
+});
+
+
